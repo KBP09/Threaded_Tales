1f9ca43e452f9ae2bd14d6dfc2a3ea553fde34fe
KBP09 <kanakbpandey2023@gmail.com>
2024-10-11 19:30:44 +0530

    []

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,3 @@
-// src/pages/Login.jsx
-
 import React, { useContext } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useNavigate } from 'react-router-dom';
--- src/pages/Profilepage.jsx
+++ src/pages/Profilepage.jsx
@@ -61,7 +61,7 @@ const ProfilePage = () => {
               </div>
               <div className="text-center mt-12">
                 <h3 className="text-4xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
-                  Jenna Stones
+                  Kanak B Pandey
                 </h3>
                 <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
                   <i className="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>
@@ -80,7 +80,7 @@ const ProfilePage = () => {
                 <div className="flex flex-wrap justify-center">
                   <div className="w-full lg:w-9/12 px-4">
                     <p className="mb-4 text-lg leading-relaxed text-blueGray-700">
-                      An artist of considerable range, Jenna the name taken by
+                      An artist of considerable range, Kanak the name taken by
                       Melbourne-raised, Brooklyn-based Nick Murphy writes,
                       performs and records all of his own music, giving it a
                       warm, intimate feel with a solid groove structure. An
